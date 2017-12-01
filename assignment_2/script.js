01a9b630a7e2a1b15c90efbacf1a4f65ab627b2b
jesper.nasman <jesper.nasman@gmail.com>
2017-12-01 15:24:46 +0100

    Added simple mean plot

--- assignment_2/script.js
+++ assignment_2/script.js
@@ -6,43 +6,68 @@ d3.select(window).on('load', init);
 
 function init() {
 
+  var margin = {top: 20, right: 20, bottom: 30, left: 50},
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+
+  // parse the date / time
+  var parseTime = d3.timeParse("%d-%b-%y");
+
+  // set the ranges
+  var x = d3.scaleLinear().range([0, width]);
+  var y = d3.scaleLinear().range([height, 0]);
+
+  // define the line
+  var valueline = d3.line()
+      .x(function(d) { return x(d.YEAR); })
+      .y(function(d) { return y(d.metANN); });
+
+  // append the svg obgect to the body of the page
+  // appends a 'group' element to 'svg'
+  // moves the 'group' element to the top left margin
   var svg = d3.select("body").append("svg")
-    .attr("width", 1000 + margin.left + margin.right)
-    .attr("height", 1000 + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
-  d3.csv(
-    'station_normalized.csv',
+  // Get the data
+  d3.csv("station_normalized.csv", function(error, data) {
+    if (error) throw error;
 
-    function(error, csv) {
-      if (error) throw error;
 
-      var i = 0;
-
-      d3.select('body')
-        .append('ul')
-        .selectAll('li')
-        .data(csv)
-        .enter()
-        .append('li')
-        .text(function(d){
-          i = i + 1;
-          console.log(i);
-          return d.YEAR + ' '
-               + d.JAN + ' '
-               + d.FEB + ' '
-               + d.MAR + ' '
-               + d.APR + ' '
-               + d.MAY + ' '
-               + d.JUN + ' '
-               + d.JUL + ' '
-               + d.AUG + ' '
-               + d.SEP + ' '
-               + d.OCT + ' '
-               + d.NOV + ' '
-               + d.DEC + ' '
-               ;
-        });
+    data.forEach(function(d) {
+        d.YEAR = +d.YEAR;
+        d.JAN = +d.JAN;
     });
-}
+
+    // Scale the range of the data
+    x.domain(d3.extent(data, function(d) { return d.YEAR; }));
+    y.domain([d3.min(data, function(d) { return d.metANN; }), d3.max(data, function(d) { return d.metANN; })]);
+
+    // Add the valueline path.
+    svg.append("path")
+        .data([data])
+        .attr("class", "line")
+        .attr("d", valueline);
+
+    // Add the scatterplot
+    svg.selectAll("dot")
+        .data(data)
+      .enter().append("circle")
+        .attr("r", 5)
+        .attr("cx", function(d) { return x(d.YEAR); })
+        .attr("cy", function(d) { return y(d.metANN); });
+
+    // Add the X Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // Add the Y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+});
+};
