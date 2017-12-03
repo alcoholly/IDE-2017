0d732e13eed663415ce78ff6fd319b8d5fa6d8d4
Io Ark <io.arkoudi@gmail.com>
2017-12-03 19:27:18 +0200

    Added scatterplot in js Erased the data before 1917 from csv files

--- assignment_2/script.js
+++ assignment_2/script.js
@@ -6,68 +6,119 @@ d3.select(window).on('load', init);
 
 function init() {
 
-  var margin = {top: 20, right: 20, bottom: 30, left: 50},
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+    // FIRST PLOT
 
-  // parse the date / time
-  var parseTime = d3.timeParse("%d-%b-%y");
+    var margin1 = {top: 20, right: 20, bottom: 30, left: 50},
+        width1 = 960 - margin1.left - margin1.right,
+        height1 = 500 - margin1.top - margin1.bottom;
 
-  // set the ranges
-  var x = d3.scaleLinear().range([0, width]);
-  var y = d3.scaleLinear().range([height, 0]);
-
-  // define the line
-  var valueline = d3.line()
-      .x(function(d) { return x(d.YEAR); })
-      .y(function(d) { return y(d.metANN); });
-
-  // append the svg obgect to the body of the page
-  // appends a 'group' element to 'svg'
-  // moves the 'group' element to the top left margin
-  var svg = d3.select("body").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
-
-  // Get the data
-  d3.csv("station_normalized.csv", function(error, data) {
-    if (error) throw error;
+    var x1 = d3.scaleLinear().range([0, width1]);
+    var y1 = d3.scaleLinear().range([height1, 0]);
 
 
-    data.forEach(function(d) {
-        d.YEAR = +d.YEAR;
-        d.JAN = +d.JAN;
+    // append the svg object to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+
+    var div = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+
+
+    var svg1 = d3.select("#scatter").append("svg")
+        .attr("width", width1 + margin1.left + margin1.right)
+        .attr("height", height1 + margin1.top + margin1.bottom)
+        .append("g") // The ‘g’ element is a container element for grouping together related graphics elements.
+        .attr("transform",
+            "translate(" + margin1.left + "," + margin1.top + ")");
+
+
+    d3.csv("station_normalized.csv", function (error, data) {
+        if (error) throw error;
+
+
+        // Scale the range of the data
+        x1.domain([d3.min(data, function(d){ return +d.YEAR;})-2, d3.max(data, function(d) {return +d.YEAR;})]);
+        y1.domain([d3.min(data, function(d){ return +d.metANN;})-0.25, d3.max(data, function(d) {return +d.metANN;})]);
+
+        // Add the scatterplot
+        svg1.selectAll("dot")
+            .data(data)
+            .enter().append("circle")
+            .attr("r", 4)
+            .attr("class", "dot")
+            .attr("cx", function(d) { return x1(d.YEAR); }) //set x nd y locations
+            .attr("cy", function(d) { return y1(d.metANN); })
+            .on("mouseover", function(d) {
+                div.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                div.html(d.YEAR + "<br/>" + d.metANN)
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function(d) {
+                div1.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
+        // Add the X Axis
+        svg1.append("g")
+            .attr("transform", "translate(0," + height1 + ")")
+            .call(d3.axisBottom(x1));
+
+        // Add the Y Axis
+        svg1.append("g")
+            .call(d3.axisLeft(y1));
+
     });
+       // SECOND PLOT
 
-    // Scale the range of the data
-    x.domain(d3.extent(data, function(d) { return d.YEAR; }));
-    y.domain([d3.min(data, function(d) { return d.metANN; }), d3.max(data, function(d) { return d.metANN; })]);
+    var margin = {top: 0, right: 0, bottom: 0, left: 25},
+        width = parseFloat(d3.select('body').style('width')) - 20;
+    height = 500;
 
-    // Add the valueline path.
-    svg.append("path")
-        .data([data])
-        .attr("class", "line")
-        .attr("d", valueline);
 
-    // Add the scatterplot
-    svg.selectAll("dot")
-        .data(data)
-      .enter().append("circle")
-        .attr("r", 5)
-        .attr("cx", function(d) { return x(d.YEAR); })
-        .attr("cy", function(d) { return y(d.metANN); });
+    var y = d3.scaleLinear().range([height, 0]);
+    var x = d3.scaleBand().range([0, width]);
 
-    // Add the X Axis
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
+    var xAxis = d3.axisBottom(x);
+    var yAxis = d3.axisLeft(y);
 
-    // Add the Y Axis
-    svg.append("g")
-        .call(d3.axisLeft(y));
+    var svg = d3.select("#barchart")
+        .append("g")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-});
-};
+    d3.csv("decade_means.csv", function(data) {
+
+        data.forEach(function(d){
+            d.decade = d.decade + '-' + d.decade.replace(/0$/,"9");
+            d.mean = +d.mean;
+        });
+
+        x.domain(data.map(function(d) { return d.decade; }))
+            .paddingInner(0.1)
+            .paddingOuter(0.2);
+        y.domain([0, d3.max(data, function(d) { return d.mean; }) + 0.5]);
+
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .call(yAxis)
+
+        svg.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("data-mean", function(d) { return d.mean; })
+            .attr("x", function(d) { return x(d.decade); })
+            .attr("width", x.bandwidth())
+            .attr("y", function(d) { return y(d.mean); })
+            .attr("height", function(d) { return height - y(d.mean); });
+    });
+}
