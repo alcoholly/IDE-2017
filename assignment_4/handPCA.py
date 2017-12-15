758a5ad684a9faece1e376ed04a2d5b5273d3968
jesper.nasman <jesper.nasman@gmail.com>
2017-12-15 13:16:19 +0100

    Updated assignment 4

--- assignment_4/handPCA.py
+++ assignment_4/handPCA.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from sklearn.decomposition import PCA
+import csv
 
 # Load csv file
 hands     = np.loadtxt('hands.csv', delimiter=',')
@@ -9,11 +10,15 @@ hands     = np.loadtxt('hands.csv', delimiter=',')
 # Perform PCA
 hands_pca = PCA().fit_transform(hands)
 
-# If there are fewer points than dimensions the PCA will automatically remove some columns. 
+# If there are fewer points than dimensions the PCA will automatically remove some columns.
 # They're not needed, but for completeness I readd them
 if hands.shape[0]<hands.shape[1]:
     hands_pca = np.append(hands_pca, np.zeros( (hands.shape[0],hands.shape[1]-hands.shape[0])),axis=1)
 
 # Store PCA
-np.savetxt('hands_pca.csv',hands_pca,fmt="%11.8f", delimiter=',')
 
+with open('hands_pca.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(['x','y'])
+    for line in [h[:2] for h in hands_pca]:
+        writer.writerow(line)
--- assignment_4/script.js
+++ assignment_4/script.js
@@ -1,9 +1,131 @@
+// Some inspiration taken from https://bl.ocks.org/dimitardanailov/6f0a451d4457b9fa7bf6e0dddcd0f468
+function drawSmallHands(){
+  d3.text('hands.csv', function(text) {
+      var data = d3.csvParseRows(text);
 
+      var drawHandLine = d3.line()
+        .x(function(d) { return d.x1*55; })
+        .y(function(d) { return d.y1*55; })
+        .curve(d3.curveBasis);
 
-d3.select(window).on('load', init);
+      data.forEach(function(d, i){
+        hand = getHandCoordinates(d)
 
+        d3.select("#plotarea").append('div')
+          .attr('class', 'small_hand_div')
+          .attr('data-id', i)
+          .append('svg')
+          .attr('class', 'small_hand')
+          .selectAll("path")
+          .data(hand)
+          .enter()
+          .append("path")
+          .attr("fill", 'beige')
+          .attr("d", drawHandLine(hand))
+          .attr("stroke", "black")
+          .attr("stroke-width", 0.1)
+          .attr('transform', "scale(1.05)")
+      });
+  });
+}
 
-function init() {
+function drawBigHand(index){
+  d3.text('hands.csv', function(text) {
+
+      var data = d3.csvParseRows(text);
+
+      var drawHandLine = d3.line()
+        .x(function(d) { return d.x1*350; })
+        .y(function(d) { return d.y1*350; })
+        .curve(d3.curveBasis);
+
+        hand = getHandCoordinates(data[index])
+
+        d3.select("#big_hand").remove();
+        d3.select("#plotarea2").append('svg')
+          .attr('id', 'big_hand')
+          .selectAll("path")
+          .data(hand)
+          .enter()
+          .append("path")
+          .attr("fill", 'beige')
+          .attr("d", drawHandLine(hand))
+          .attr("stroke", "black")
+          .attr("stroke-width", 0.1)
+          .attr('transform', "scale(1.05)")
+  });
+}
+
+function scatterPlot(){
+  var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 860 - margin.left - margin.right,
+    height = 700 - margin.top - margin.bottom;
+
+    // parse the date / time
+
+    // set the ranges
+    var x = d3.scaleLinear().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+
+    // define the line
+
+    // append the svg obgect to the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    var svg = d3.select("#scatter").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
+
+    // Get the data
+    d3.csv("hands_pca.csv", function(error, data) {
+      if (error) throw error;
+
+      // format the data
+      data.forEach(function(d) {
+          d.x = +d.x;
+          d.y = +d.y;
+      });
+
+      // Scale the range of the data
+      x.domain(d3.extent(data, function(d) { return d.x; }));
+      y.domain([0, d3.max(data, function(d) { return d.y; })]);
+
+      // Add the valueline path.
+      svg.append("path")
+          .data([data])
+          .attr("class", "line")
+
+      // Add the scatterplot
+      svg.selectAll("dot")
+
+          .data(data)
+          .enter()
+          .append("circle")
+          .attr("r", 5)
+          .attr('class', 'plot_circle')
+          .attr('data-id', 'something')
+          .attr("cx", function(d) { return x(d.x); })
+          .attr("cy", function(d) { return y(d.y); })
+          .style("fill", 'black')
+          .each(function (d, i) {
+            d3.select(this).attr('data-id', i);
+          });
+
+      // Add the X Axis
+      svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
+
+      // Add the Y Axis
+      svg.append("g")
+          .call(d3.axisLeft(y));
+
+    });
+}
+
 function getHandCoordinates(data){
   coords = []
   for (var i = 0; i < 56; i++){
@@ -13,46 +135,21 @@ function getHandCoordinates(data){
 }
 
 
+$(document).on('click', '.small_hand_div', function () {
+  dataid = $(this).attr('data-id');
+  $('.plot_circle').css('fill','black');
+  $('.plot_circle').attr('r','5');
+  $('.plot_circle[data-id=' + dataid + ']').css('fill','red');
+  $('.plot_circle[data-id=' + dataid + ']').attr('r','10');
+  drawBigHand($(this).attr('data-id'));
+});
 
-function draw(data){}
+$(document).on('click', '.plot_circle', function () {
+  drawBigHand($(this).attr('data-id'));
+});
 
-
-// Some inspiration taken from https://bl.ocks.org/dimitardanailov/6f0a451d4457b9fa7bf6e0dddcd0f468
-
-function drawSmallHands(){
-  d3.text('hands.csv', function(text) {
-      var data = d3.csvParseRows(text);
-
-      var drawHandLine = d3.line()
-        .x(function(d) { return d.x1*55; })
-        .y(function(d) { return d.y1*55; })
-
-      data.forEach(function(d){
-        hand = getHandCoordinates(d)
-
-        d3.select("#plotarea").append('div').append('svg')
-          .attr('class', 'lilbox')
-          .selectAll("path")
-          .data(hand)
-          .enter()
-          .append("path")
-          .attr("fill", 'beige')
-          .attr("d", drawHandLine(hand))
-          .attr("stroke", "black")
-          .attr("stroke-width", 0.2)
-          .attr('transform', "scale(1.05)")
-
-
-      });
-
-
-  });
-}
-
-drawSmallHands();
-
-var mydata = [[10,4,9,3],[9,3,8,3],[8,3,7,2],[7,2,7,1],[7,1,6,1],[6,1,6,2],[6,2,7,3],[7,3,7,4],[7,4,6,4]]
-
-
-
-}
+d3.select(window).on('load', function(){
+  drawSmallHands();
+  drawBigHand(0);
+  scatterPlot();
+});
